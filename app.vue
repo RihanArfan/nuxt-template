5f0d2be25f44634b5f5299b08fd13130bbd26ea8
Rihan Arfan <me@file.properties>
2024-04-11 02:41:08 +0100

    chore: update template

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLoadingIndicator />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "nuxt-icon",
+    "@vueuse/nuxt",
+  ],
+
+  experimental: {
+    typedPages: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  devtools: { enabled: true },
+});
